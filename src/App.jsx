537b27da34930b2600177ca134f20a46eececedd
MohamedAli09 <mohamed21-01166@student.eelu.edu.eg>
2024-03-02 15:45:19 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -25,8 +25,10 @@ export default function App() {
   }, []);
 
   let routes = createBrowserRouter([
+    
     {
       path: "",
+      basename: "E-Commerce",
       element: <Layout />,
       children: [
         {
